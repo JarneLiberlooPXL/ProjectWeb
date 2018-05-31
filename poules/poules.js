6bfd5092511f35117dcf4b341ac46cca66e38769
borcherd <32452429+borcherd@users.noreply.github.com>
2018-05-31 09:36:36 +0200

    Add files via upload

---
+++ poules/poules.js
@@ -0,0 +1,50 @@
+let poules = {
+    'A': [
+        {land: "Egypte", vlag: "landen/egy.png"},
+        {land: "Rusland", vlag: "landen/rus.png"},
+        {land: "Saudi-Arabië", vlag: "landen/sar.png"},
+        {land: "Uruguay", vlag: "landen/uru.png"}
+    ],
+    'B': [
+        {land: "Iran", vlag: "landen/irn.png"},
+        {land: "Marokko", vlag: "landen/mor.png"},
+        {land: "Portugal", vlag: "landen/por.png"},
+        {land: "Spanje", vlag: "landen/esp.png"}
+    ],
+    'C': [
+        {land: "Australië", vlag: "landen/aus.png"},
+        {land: "Denemarken", vlag: "landen/den.png"},
+        {land: "Frankrijk", vlag: "landen/fra.png"},
+        {land: "Peru", vlag: "landen/per.png"}
+    ],
+    'D': [
+        {land: "Argentinië", vlag: "landen/arg.png"},
+        {land: "Kroatië", vlag: "landen/cro.png"},
+        {land: "Ijsland", vlag: "landen/isl.png"},
+        {land: "Nigeria", vlag: "landen/nig.png"}
+    ],
+    'E': [
+        {land: "Brazilië", vlag: "landen/bra.png"},
+        {land: "Costa Rica", vlag: "landen/crc.png"},
+        {land: "Zwitserland", vlag: "landen/sui.png"},
+        {land: "Servië", vlag: "landen/srb.png"}
+    ],
+    'F': [
+        {land: "Duitsland", vlag: "landen/ger.png"},
+        {land: "Zuid-Korea", vlag: "landen/kor.png"},
+        {land: "Mexico", vlag: "landen/mex.png"},
+        {land: "Zweden", vlag: "landen/swe.png"}
+    ],
+    'G': [
+        {land: "België", vlag: "landen/bel.png"},
+        {land: "Engeland", vlag: "landen/eng.png"},
+        {land: "Panama", vlag: "landen/pan.png"},
+        {land: "Tunesië", vlag: "landen/tun.png"}
+    ],
+    'H': [
+        {land: "Colombia", vlag: "landen/col.png"},
+        {land: "Japan", vlag: "landen/jap.png"},
+        {land: "Polen", vlag: "landen/pol.png"},
+        {land: "Senegal", vlag: "landen/sen.png"}
+    ]
+};
